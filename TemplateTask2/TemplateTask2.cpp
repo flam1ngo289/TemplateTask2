d54218b8a615416404c3dcc8a11499e07ec00dd5
flam1ngo <kris.s.03@mail.ru>
2024-05-25 14:28:22 +0300

    Initial commit

---
+++ TemplateTask2/TemplateTask2.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+
+template <class T>
+class table
+{
+private:
+    class table_two;
+    int row, col;
+    table_two* arrint;
+    class table_two
+    {
+    public:
+        int* sarrint;
+        table_two() {}
+        table_two(int size)
+        {
+            sarrint = new int[size];
+        }
+        int& operator [] (int i)
+        {
+            return sarrint[i];
+        }
+    };
+
+public:
+    table(const int rows, const int cols)
+    {
+        rows < 1 ? row = 1 : row = rows;
+        cols < 1 ? col = 1 : col = cols;
+        arrint = new table_two[row];
+        for (int i = 0; i < row; i++)
+        {
+            arrint[i] = table_two(col);
+        }
+    }
+
+    table& operator=(const table& copyFrom) {
+        if (this != &copyFrom) {
+            for (int i = 0; i < row; i++)
+            {
+                if (arrint[i].sarrint != nullptr)
+                    delete[](arrint[i].sarrint);
+            }
+            if (arrint != nullptr)
+                delete[] arrint;
+            this->row = copyFrom.row;
+            this->col = copyFrom.col;
+            this->arrint = new table_two[row];
+            for (int i = 0; i < row; i++)
+            {
+                this->arrint[i] = table_two(col);
+            }
+            for (int i = 0; i < row; ++i) {
+                for (int j = 0; j < col; ++j) {
+                    this->arrint[i][j] = copyFrom.arrint[i][j];
+                }
+            }
+            return *this;
+        }
+        else {
+            return *this;
+        }
+    }
+
+    table(const table& copyFrom) {
+        this->row = copyFrom.row;
+        this->col = copyFrom.col;
+        this->arrint = new table_two[row];
+        for (int i = 0; i < row; i++)
+        {
+            this->arrint[i] = table_two(col);
+        }
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                this->arrint[i][j] = copyFrom.arrint[i][j];
+            }
+        }
+    }
+
+    table_two& operator [] (int i)
+    {
+        return arrint[i];
+    }
+    ~table()
+    {
+        for (int i = 0; i < row; i++)
+        {
+            if (arrint[i].sarrint != nullptr)
+                delete[](arrint[i].sarrint);
+        }
+        if (arrint != nullptr)
+            delete[] arrint;
+    }
+};
+int main()
+{
+    auto test = table<int>(2, 3);
+    auto new_test = test;
+
+    test[0][0] = 4;
+    std::cout << test[0][0] << std::endl; // выводит 4
+    system("Pause");
+    return 0;
+}

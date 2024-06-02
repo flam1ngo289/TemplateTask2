2d6c37a6383d8663a84243883e7dabb4407387fe
flam1ngo <kris.s.03@mail.ru>
2024-06-02 11:11:37 +0300

    Third commit

--- TemplateTask2/TemplateTask2.cpp
+++ TemplateTask2/TemplateTask2.cpp
@@ -1,119 +1,110 @@
 ﻿#include <iostream>
-#include <tuple> 
+#include <tuple>
 #include <Windows.h>
 
 template<class T>
-class table
-{
+class table {
 private:
     class table_two;
     int row, col;
-    table_two* arrint;
-    class table_two
-    {
+    table_two* arr;
+
+    class table_two {
     public:
-        int* sarrint;
-        table_two() {}
-        table_two(int size)
-        {
-            sarrint = new int[size];
+        T* sarr;
+        table_two() : sarr(nullptr) {}
+        table_two(int size) {
+            sarr = new T[size];
         }
-        int& operator [] (int i)
-        {
-            return sarrint[i];
+       
+
+        T& operator [] (int i) {
+            return sarr[i];
         }
+
+        const T& operator [] (int i) const {
+            return sarr[i];
+        }
+
     };
 
 public:
-    table(const int rows, const int cols)
-    {
-        rows < 1 ? row = 1 : row = rows;
-        cols < 1 ? col = 1 : col = cols;
-        arrint = new table_two[row];
-        for (int i = 0; i < row; i++)
-        {
-            arrint[i] = table_two(col);
+    table(const int rows, const int cols) {
+        row = (rows < 1) ? 1 : rows;
+        col = (cols < 1) ? 1 : cols;
+        arr = new table_two[row];
+        for (int i = 0; i < row; ++i) {
+            arr[i] = table_two(col);
+        }
+    }
+
+    table(const table& copyFrom) {
+        row = copyFrom.row;
+        col = copyFrom.col;
+        arr = new table_two[row];
+        for (int i = 0; i < row; ++i) {
+            arr[i] = table_two(col);
+            for (int j = 0; j < col; ++j) {
+                arr[i][j] = copyFrom.arr[i][j];
+            }
         }
     }
 
     table& operator=(const table& copyFrom) {
         if (this != &copyFrom) {
-            for (int i = 0; i < row; i++)
-            {
-                if (arrint[i].sarrint != nullptr)
-                    delete[](arrint[i].sarrint);
-            }
-            if (arrint != nullptr)
-                delete[] arrint;
-            this->row = copyFrom.row;
-            this->col = copyFrom.col;
-            this->arrint = new table_two[row];
-            for (int i = 0; i < row; i++)
-            {
-                this->arrint[i] = table_two(col);
-            }
             for (int i = 0; i < row; ++i) {
+                delete[] arr[i].sarr;
+            }
+            delete[] arr;
+
+            row = copyFrom.row;
+            col = copyFrom.col;
+            arr = new table_two[row];
+            for (int i = 0; i < row; ++i) {
+                arr[i] = table_two(col);
                 for (int j = 0; j < col; ++j) {
-                    this->arrint[i][j] = copyFrom.arrint[i][j];
+                    arr[i][j] = copyFrom.arr[i][j];
                 }
             }
-            return *this;
         }
-        else {
-            return *this;
-        }
+        return *this;
     }
 
-    table(const table& copyFrom) {
-        this->row = copyFrom.row;
-        this->col = copyFrom.col;
-        this->arrint = new table_two[row];
-        for (int i = 0; i < row; i++)
-        {
-            this->arrint[i] = table_two(col);
-        }
-        for (int i = 0; i < row; ++i) {
-            for (int j = 0; j < col; ++j) {
-                this->arrint[i][j] = copyFrom.arrint[i][j];
-            }
-        }
+    table_two& operator [] (int i) {
+        return arr[i];
     }
 
-    table_two& operator [] (int i)
-    {
-        return arrint[i];
+    const table_two& operator [] (int i) const {
+        return arr[i];
     }
 
-    std::tuple<int, int> Size() const 
-    {
+    std::tuple<int, int> Size() const {
         return std::make_tuple(row, col);
     }
 
-    ~table()
-    {
-        for (int i = 0; i < row; i++)
-        {
-            if (arrint[i].sarrint != nullptr)
-                delete[](arrint[i].sarrint);
+    ~table() {
+        for (int i = 0; i < row; ++i) {
+            delete[] arr[i].sarr;
         }
-        if (arrint != nullptr)
-            delete[] arrint;
+        delete[] arr;
     }
+
+   
 };
 
-int main()
-{
+int main() {
+
     setlocale(LC_ALL, "RU");
 
     auto test = table<int>(2, 3);
     auto new_test = test;
 
     test[0][0] = 4;
-    std::cout << test[0][0] << std::endl; 
+    std::cout << test[0][0] << std::endl;
 
     int rows, cols;
-    std::tie(rows, cols) = test.Size(); 
-    std::cout << "Строки: " << rows << ", Столбцы: " << cols << std::endl; // выводит размеры таблицы
+    std::tie(rows, cols) = test.Size();
+    std::cout << "Строки: " << rows << ", Столбцы: " << cols << std::endl;
 
     return 0;
 }
